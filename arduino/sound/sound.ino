259fdffa07b6e0d90ce2fd64d4b1356be3b04000
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-07-02 18:46:34 -0500

    Add files via upload

--- arduino/sound/sound.ino
+++ arduino/sound/sound.ino
@@ -15,13 +15,3 @@ void setup()
 void loop()
 {
 }
-
-// Speed of Light: 299,792,458 meters per second - https://www.space.com/speed-of-light-properties-explained.html
-
-// Speed of Sound in Air: - 343 meters per second - https://en.wikipedia.org/wiki/Speed_of_sound
-
-// Water Resonance: 22.235 GHz https://ntrs.nasa.gov/citations/19720031225
-
-// 299,792,458 m / 2,450,000,000 Hz = 0.013 m wave size
-// 343 m / 0.013 = 2,803.20366133 hertz
-// 343 m /  26,384.615 Hz = 0.12236 m wave size
